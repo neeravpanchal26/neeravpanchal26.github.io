2c1db95fce4482b6954b45a876d4a26b88949639
Neerav Panchal <masterpanchieneerav@gmail.com>
2020-05-10 16:14:05 +0200

    10-05 2.0

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-// Custom imports
-import {createStore, applyMiddleware} from "redux";
-import thunkMiddleware from "redux-thunk";
-import Reducer from "../reducers/reducer";
-
-export default function configureStore(preloadedState) {
-    return createStore(Reducer, preloadedState, applyMiddleware(thunkMiddleware));
-}
