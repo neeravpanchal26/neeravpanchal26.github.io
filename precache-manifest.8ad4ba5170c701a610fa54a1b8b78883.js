97f5eb9324aa50e69afd67cfa953bf46136ada32
Neerav Panchal <masterpanchieneerav@gmail.com>
2020-08-18 21:21:58 +0200

    18-09-20
    Silent Dark Mode release

---
+++ precache-manifest.8ad4ba5170c701a610fa54a1b8b78883.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79d8e48614c91444d40d37153fabd43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10cac5d4b83c12802c78",
+    "url": "/static/css/2.135b7c2a.chunk.css"
+  },
+  {
+    "revision": "0b990d6d862378a64120",
+    "url": "/static/css/main.bb0af0f9.chunk.css"
+  },
+  {
+    "revision": "10cac5d4b83c12802c78",
+    "url": "/static/js/2.50caf0a2.chunk.js"
+  },
+  {
+    "revision": "b9a522886c0ef5b10ec9a2c0a23be328",
+    "url": "/static/js/2.50caf0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b990d6d862378a64120",
+    "url": "/static/js/main.46ffeb93.chunk.js"
+  },
+  {
+    "revision": "7fd50307644dd3303e46",
+    "url": "/static/js/runtime-main.bfcebc03.js"
+  },
+  {
+    "revision": "0c7e5ba541bde68a1489fa1188985a23",
+    "url": "/static/media/IMG_2778.0c7e5ba5.jpg"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ae62ed88b49366bf58a3884f617e39c.js"
+  "/precache-manifest.8ad4ba5170c701a610fa54a1b8b78883.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
